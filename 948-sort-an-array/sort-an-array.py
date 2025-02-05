dc42ea8e43da2050b8e28b84e6e9d4c59bf4191f
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-02-05 12:00:21 -0600

    Time: 719 ms (37.65%) | Memory: 32.5 MB (10.58%) - LeetSync

--- 948-sort-an-array/sort-an-array.py
+++ 948-sort-an-array/sort-an-array.py
@@ -1,8 +1,9 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge(arr, L, M, R):
-            left, right = arr[L:M+1], arr[M+1: R+1]
-            i, j, k = L, 0, 0
+        def mergesort(arr, L, M, R):
+            left, right = arr[L:M + 1], arr[M+1:R+1]
+
+            i,j,k = L, 0, 0
 
             while j < len(left) and k < len(right):
                 if left[j] <= right[k]:
@@ -11,7 +12,7 @@ class Solution:
                 else:
                     arr[i] = right[k]
                     k += 1
-                i += 1
+                i +=1
             while j < len(left):
                 nums[i] = left[j]
                 j += 1
@@ -20,15 +21,14 @@ class Solution:
                 nums[i] = right[k]
                 k += 1
                 i += 1
-
-        def mergeSort(arr, l, r):
+        def merge(arr, l, r):
             if l == r:
                 return
-            m = (l + r) // 2
-            mergeSort(arr, l, m)
-            mergeSort(arr, m + 1, r)
-            merge(arr, l, m, r)
+            m = ( l + r ) // 2
+            merge(arr, l, m)
+            merge(arr, m + 1, r)
+            mergesort(arr, l , m , r)
 
-        mergeSort(nums, 0, len(nums))
+        merge(nums, 0, len(nums))
+
         return nums
-               
