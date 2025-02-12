e2c231d0066db26fc38b0901638a331a1fa0edc4
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-02-11 22:05:48 -0600

    Time: 2 ms (95.90%) | Memory: 18.8 MB (81.54%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -8,12 +8,11 @@ class Solution:
                 if diff < 0:
                     stack.pop()
                 elif diff > 0:
-                    a = 0
+                    a = 0 
                 else:
-                    a = 0
+                    a = 0 
                     stack.pop()
 
             if a:
                 stack.append(a)
-
         return stack
