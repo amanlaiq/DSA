39fc73cebb5f145980b306149b1d36328b1c373a
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-02 15:22:13 -0500

    Time: 162 ms (33.43%) | Memory: 78.3 MB (63.25%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -2,7 +2,8 @@ class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
-        self.prev, self.next = None, None
+        self.prev = None
+        self.next = None
 
 class LRUCache:
 
@@ -10,18 +11,18 @@ class LRUCache:
         self.cap = capacity
         self.cache = {}
 
-        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left, self.right = Node(0, 0), Node(0,0)
         self.left.next, self.right.prev = self.right, self.left
 
     def remove(self, node):
         prev, nxt = node.prev, node.next
-        prev.next = nxt
-        nxt.prev = prev
+        prev.next, nxt.prev = nxt, prev
 
     def insert(self, node):
         prev, nxt = self.right.prev, self.right
         prev.next = nxt.prev = node
-        node.prev, node.next = prev, nxt
+        node.prev = prev
+        node.next = nxt
 
     def get(self, key: int) -> int:
         if key in self.cache:
@@ -29,19 +30,18 @@ class LRUCache:
             self.insert(self.cache[key])
             return self.cache[key].val
         return -1
-
+        
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
             self.remove(self.cache[key])
-
+        
         self.cache[key] = Node(key, value)
         self.insert(self.cache[key])
-
+        
         if len(self.cache) > self.cap:
             lru = self.left.next
             self.remove(lru)
-            del self.cache[lru.key]  # Remove from hashmap
-
+            del self.cache[lru.key]
         
 
 
