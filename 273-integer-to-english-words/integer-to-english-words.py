4db79a0891eed999b70c48a51215497a6923eca1
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-03 13:58:52 -0500

    Time: 1 ms (38.23%) | Memory: 18 MB (25.72%) - LeetSync

--- 273-integer-to-english-words/integer-to-english-words.py
+++ 273-integer-to-english-words/integer-to-english-words.py
@@ -2,37 +2,37 @@ class Solution:
     def numberToWords(self, num: int) -> str:
         if num == 0:
             return "Zero"
-        
-        bigString = ["Thousand", "Million", "Billion"]
-        result = self.numberToWordsHelper(num % 1000)
-        num //= 1000
-        
-        for i in range(len(bigString)):
-            if num > 0 and num % 1000 > 0:
-                result = self.numberToWordsHelper(num % 1000) + bigString[i] + " " + result
-            num //= 1000
-        
+
+        bigstring = ["Thousand","Million","Billion"]
+        result = self.notoword(num % 1000)
+        num = num // 1000
+
+        for i in range(len(bigstring)):
+            if num % 1000 > 0:
+                result = self.notoword(num % 1000) + bigstring[i] + " " + result
+            num = num // 1000
+
         return result.strip()
 
-    def numberToWordsHelper(self, num: int) -> str:
+    def notoword(self, num):
         digitString = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
         teenString = ["Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"]
         tenString = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"]
-        
-        #handle hundreds
+
         result = ""
         if num > 99:
             result += digitString[num // 100] + " Hundred "
         
-        #reduce number to below hundred
-        num %= 100
-        if num < 20 and num > 9:
+        num = num % 100
+
+        if num > 9 and num < 20:
             result += teenString[num - 10] + " "
         else:
             if num >= 20:
                 result += tenString[num // 10] + " "
-            num %= 10
+            num = num % 10
             if num > 0:
                 result += digitString[num] + " "
-        
-        return result
+        return result 
+
+
