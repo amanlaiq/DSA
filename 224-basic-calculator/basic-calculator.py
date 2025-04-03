89eab3a0c914ffb7b982373bb480617f588d2911
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-02 21:43:39 -0500

    Time: 75 ms (11.55%) | Memory: 75.8 MB (5.03%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.py
@@ -0,0 +1,25 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        def update(op, v):
+            if op == "+": stack.append(v)
+            if op == "-": stack.append(-v)
+            if op == "*": stack.append(stack.pop() * v)
+            if op == "/": stack.append(int (stack.pop() / v))
+        i, num, stack, sign = 0, 0, [], "+"
+        
+        while i < len(s):
+            if s[i].isdigit():
+                num = num * 10 + int(s[i])
+            elif s[i] in "+-*/":
+                update(sign, num)
+                num = 0
+                sign = s[i]
+            elif s[i] == "(":
+                num, j = self.calculate(s[i + 1:])
+                i = i + j
+            elif s[i] == ")":
+                update(sign, num)
+                return sum(stack), i + 1
+            i += 1
+        update(sign, num)
+        return sum(stack)
