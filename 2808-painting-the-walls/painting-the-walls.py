a3ac543f2a11b5f044bf87938df6b2e1e8a1d486
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-03 10:17:49 -0500

    Time: 869 ms (93.90%) | Memory: 17.7 MB (97.97%) - LeetSync

--- 2808-painting-the-walls/painting-the-walls.py
+++ 2808-painting-the-walls/painting-the-walls.py
@@ -6,4 +6,16 @@ class Solution:
             for j in range(n, 0, -1):
                 dp[j] = min(dp[j], dp[max(j - (t + 1), 0)] + c)
         return dp[n]
+
+
+
+
+# class Solution:
+#     def paintWalls(self, cost: List[int], time: List[int]) -> int:
+#         n = len(cost)
+#         dp = [0] + [inf] * n
+#         for c, t in zip(cost, time):
+#             for j in range(n, 0, -1):
+#                 dp[j] = min(dp[j], dp[max(j - (t + 1), 0)] + c) #the question I am asking here is, can I paint j walls by choosing this particular wall ie t + 1
+#         return dp[n]
     
