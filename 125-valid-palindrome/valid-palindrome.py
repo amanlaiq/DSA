256b0f0bf4a61809bfadcd8652e528a0e85b757d
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-10 09:15:22 -0500

    Time: 28 ms (5.07%) | Memory: 18.2 MB (58.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -5,8 +5,9 @@ class Solution:
         while l < r:
             while l < r and not self.alphanum(s[l]):
                 l += 1
-            while r > l and not self.alphanum(s[r]):
+            while l < r and not self.alphanum(s[r]):
                 r -= 1
+
             if s[l].lower() != s[r].lower():
                 return False
             
@@ -14,8 +15,9 @@ class Solution:
             r -= 1
         return True
 
-
     def alphanum(self, c):
-        return ( ord('A') <= ord(c) <= ord('Z') or
-                 ord('a') <= ord(c) <= ord('z') or
-                 ord('0') <= ord(c) <= ord('9'))
+        return ( 
+            ord("A") <= ord(c) <= ord("Z") or
+            ord("a") <= ord(c) <= ord("z") or
+            ord("0") <= ord(c) <= ord("9")
+        )
