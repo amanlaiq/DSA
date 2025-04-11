f34224ca6651538199fae7554037df8ae0d5fcbb
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-11 18:32:53 -0500

    Time: 4 ms (73.99%) | Memory: 20.8 MB (48.47%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -14,9 +14,11 @@ class Solution:
 
             left = dfs(root.left)
             right = dfs(root.right)
+
             self.res = max(self.res, 2 + left + right)
 
             return 1 + max(left, right)
 
         dfs(root)
+
         return self.res
