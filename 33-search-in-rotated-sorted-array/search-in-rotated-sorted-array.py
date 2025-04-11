032a915dd1ea616924090849065c013984ec6b40
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-11 08:45:06 -0500

    Time: 3 ms (4.31%) | Memory: 17.9 MB (98.94%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,6 +6,7 @@ class Solution:
             m = (l + r) // 2
             if target == nums[m]:
                 return m
+
             if nums[l] <= nums[m]:
                 if target > nums[m] or target < nums[l]:
                     l = m + 1
@@ -16,4 +17,4 @@ class Solution:
                     r = m - 1
                 else:
                     l = m + 1
-        return -1 
+        return -1  
