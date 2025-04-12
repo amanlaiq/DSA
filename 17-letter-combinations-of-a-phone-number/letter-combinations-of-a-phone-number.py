df9412b286fca6c547ce6c95c3297995197c29b7
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-12 17:33:05 -0500

    Time: 0 ms (100.00%) | Memory: 17.5 MB (99.10%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -10,13 +10,16 @@ class Solution:
                         "7" : "pqrs",
                         "8" : "tuv",
                         "9" : "wxyz" }
+
         def backtrack(i, curstr):
             if len(curstr) == len(digits):
                 res.append(curstr)
                 return
+
             for c in digittochar[digits[i]]:
                 backtrack(i + 1, curstr + c)
         
         if digits:
             backtrack(0, "")
+
         return res
