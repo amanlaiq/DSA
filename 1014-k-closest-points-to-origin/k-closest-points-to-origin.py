428ece2f0cfaeb7fded353621ba0c3b872fb0094
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-12 11:08:44 -0500

    Time: 75 ms (48.28%) | Memory: 23.1 MB (23.08%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -3,13 +3,16 @@ class Solution:
         minheap = []
 
         for x, y in points:
-            dist = (x**2) + (y**2)
+            dist = x**2 + y**2
             minheap.append([dist, x, y])
         heapq.heapify(minheap)
 
         res = []
+
         while k > 0:
             dist, x, y = heapq.heappop(minheap)
             res.append([x, y])
-            k -=1
+
+            k -= 1
+
         return res
