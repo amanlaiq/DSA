071874752f781c9f498dff50a9e67291bad6dafb
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-12 13:51:14 -0500

    Time: 1 ms (47.92%) | Memory: 18 MB (56.96%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -4,13 +4,15 @@ class Solution:
 
         if len(nums) == 1:
             return [nums.copy()]
-        
+
         for i in range(len(nums)):
             n = nums.pop(0)
             perms = self.permute(nums)
 
             for perm in perms:
                 perm.append(n)
+            
             res.extend(perms)
             nums.append(n)
+
         return res
