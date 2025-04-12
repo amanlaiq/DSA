40fe92c383712a2e00f08826848951ff542188ec
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-12 16:45:31 -0500

    Time: 479 ms (96.98%) | Memory: 18.2 MB (21.01%) - LeetSync

--- 79-word-search/word-search.py
+++ 79-word-search/word-search.py
@@ -7,7 +7,8 @@ class Solution:
         def dfs(r, c, i):
             if i == len(word):
                 return True
-            if ( min(r,c) < 0 or
+            if ( r < 0 or
+                 c < 0 or
                  r >= ROWS or
                  c >= COLS or
                  (r, c) in path or
