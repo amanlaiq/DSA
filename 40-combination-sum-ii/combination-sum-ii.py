4f4e3b397d9d717a61fb600c3f5e0d6a00d91434
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-12 13:41:19 -0500

    Time: 19 ms (60.54%) | Memory: 18.1 MB (14.69%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -6,17 +6,16 @@ class Solution:
         def dfs(i, cur, total):
             if total == target:
                 res.append(cur.copy())
-            if i >= len(candidates) or total > target:
                 return
-            prev = -1
+            if i >= len(candidates) or total > target:
+                return 
+            prev = - 1
             for j in range(i, len(candidates)):
                 if prev == candidates[j]:
-                    continue
+                    continue 
                 cur.append(candidates[j])
                 dfs(j + 1, cur, total + candidates[j])
                 cur.pop()
                 prev = candidates[j]
-        
         dfs(0, [], 0)
         return res
-                
