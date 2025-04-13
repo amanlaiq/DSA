d315a5a2228693b1d585cc02191eb39a36f2d4f2
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-13 10:07:19 -0500

    Time: 35 ms (56.35%) | Memory: 19.5 MB (45.56%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -17,12 +17,12 @@ class Solution:
             dfs(r, c + 1, visit, heights[r][c])
             dfs(r, c - 1, visit, heights[r][c])
         
-        for c in range(cols):
-            dfs(0, c, pac, heights[0][c])
-            dfs(rows - 1, c, atl, heights[rows - 1][c])
         for r in range(rows):
             dfs(r, 0, pac, heights[r][0])
             dfs(r, cols - 1, atl, heights[r][cols - 1])
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1, c, atl, heights[rows - 1][c])
         res = []
 
         for r in range(rows):
