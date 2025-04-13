6e071bed6184c1ead6a78471785c7a1eaeee7f78
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-13 10:33:49 -0500

    Time: 11 ms (26.86%) | Memory: 21.5 MB (86.35%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.py
+++ 130-surrounded-regions/surrounded-regions.py
@@ -3,32 +3,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        ROWS, COLS = len(board), len(board[0])
+        rows, cols = len(board), len(board[0])
 
         def capture(r, c):
-            if ( r < 0 or 
-                 c < 0 or 
-                 c == COLS or 
-                 r == ROWS or 
+            if ( r < 0 or
+                 r == rows or 
+                 c < 0 or
+                 c == cols or 
                  board[r][c] != "O"):
-                 return
+                 return 
+            
             board[r][c] = "T"
             capture(r + 1, c)
             capture(r - 1, c)
             capture(r, c + 1)
             capture(r, c - 1)
 
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (board[r][c] == "O" and (r in [0, ROWS - 1] or c in [0, COLS - 1])):
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == "O" and (r in [0, rows - 1] or c in [0, cols - 1]):
                     capture(r, c)
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 if board[r][c] == "O":
                     board[r][c] = "X"
 
-        for r in range(ROWS):
-            for c in range(COLS):
+        for r in range(rows):
+            for c in range(cols):
                 if board[r][c] == "T":
                     board[r][c] = "O"
