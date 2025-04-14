0bfd13a5d8fb0ba66f192dd00f5a9ce892eb6401
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-14 15:42:15 -0500

    Time: 325 ms (44.63%) | Memory: 60.1 MB (32.83%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,20 +1,16 @@
-from typing import List
-
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n = len(coins)
+
         dp = [[0] * (amount + 1) for _ in range(n + 1)]
-        
-        # Base Case: There is exactly 1 way to make sum 0 (by choosing no coins).
+
         for i in range(n + 1):
             dp[i][0] = 1
-        
-        # Build the DP table
-        for i in range(1, n + 1):
-            for j in range(1, amount + 1):
+
+        for i in range(1,n + 1):
+            for j in range(1,amount + 1):
                 if coins[i - 1] <= j:
-                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i-1]]
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]]
                 else:
                     dp[i][j] = dp[i - 1][j]
-        # The answer is in dp[n][amount]: number of ways to form 'amount' with all n coins.
         return dp[n][amount]
