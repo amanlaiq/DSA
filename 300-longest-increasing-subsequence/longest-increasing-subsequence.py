9be78e023e80b156538556d14c13331c6330c08c
Aman Laiq Mohammed <54764667+amanlaiq@users.noreply.github.com>
2025-04-14 10:44:15 -0500

    Time: 1767 ms (46.13%) | Memory: 18.3 MB (17.83%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,9 +1,28 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        LIS = [1] * len(nums)
+        lis = [1] * len(nums)
 
-        for i in range(len(nums) - 1, -1 , -1):
+        for i in range(len(nums) - 1, -1, -1):
             for j in range(i + 1, len(nums)):
                 if nums[i] < nums[j]:
-                    LIS[i] = max(LIS[i], LIS[j] + 1)
-        return max(LIS)
+                    lis[i] = max(lis[i], lis[j] + 1)
+        return max(lis)
+
+
+
+# from typing import List
+# from functools import lru_cache
+
+# class Solution:
+#     def lengthOfLIS(self, nums: List[int]) -> int:
+#         lis = []
+
+#         for i in nums:
+#             indx = bisect.bisect_left(lis, i)
+#             if indx == len(lis):
+#                 lis.append(i)
+#             else:
+#                 lis[indx] = i
+
+            
+#         return len(lis)
